6bacc9991ea70f4765e147749d415500c418cdb1
git config --global user.name TNorsang <thenorsang@gmail.com>
2019-02-21 00:45:57 -0500

    added bamazonCustomer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,7 +21,7 @@ connection.connect(function(err) {
 function start() {
     connection.query("SELECT * FROM products", function(err, res) {
         for (var i = 0; i < res.length; i++) {
-            console.log(res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+            console.log(res[i].item_id + " | " +res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
         }
     })   
 };
