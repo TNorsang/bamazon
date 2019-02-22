2c1e1e536d59d073f9c8b299f9251114de26f38c
Norsang <thenorsang@gmail.com>
2019-02-21 20:01:51 -0500

    added bamazonCustomerjs

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,14 +15,28 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if(err) throw err; 
 
-    start(); 
+    table();
+    
 });
 
-function start() {
+function table() {
     connection.query("SELECT * FROM products", function(err, res) {
         for (var i = 0; i < res.length; i++) {
             console.log("id : " + res[i].item_id + " | " +res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
         }
     })   
+    questions();
+};
+
+function questions() {
+    inquirer
+        .prompt({
+            name: "ID",
+            type: "input",
+            message: "Choose the item buy typing in the ID Number."
+        })
+        .then (function(answer) {
+            
+        })
 };
 
