6e37f501691f4c8e03ed9a6b8673b804cc9cb8a6
Norsang <thenorsang@gmail.com>
2019-02-23 10:20:42 -0500

    added bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,47 +1,112 @@
+// --- Dependencies ---- \\ 
+
+// --- mysql package --- \\
 var mysql = require("mysql");
+// --- inquirer package that allows developers to ask questions --- \\
 var inquirer = require("inquirer");
+// --- This is a table package that we can use to setup our list of items--- \\
 var Table = require("cli-table");
 
+//MySQL connection information
 var connection = mysql.createConnection({
+
     host: "localhost",
 
     port: 3306,
 
     user: "root",
 
-    password: "root",
+    password: "rootroot",
+    
     database: "bamazon_db"
+
 });
 
+//Connection with DB
 connection.connect(function(err) {
-    if(err) throw err; 
 
-    table();
+    if (err) throw err;
+
+    console.log("Connected as ID: " + connection.threadId);
 
 });
 
-// table = new Table({
-//     head: ['TH 1 label', 'TH 2 label'],
-//     colWidths: [100, 200]
-// });
+//Function to start bamazon and display products
+var displayProducts = function() {
 
-function table() {
+    console.log("Welcome to Bamazon! Please, find our inventory bellow.");
+
+    //Query DB
     connection.query("SELECT * FROM products", function(err, res) {
-        for (var i = 0; i < res.length; i++) {
-            console.log("id : " + res[i].item_id + " | " +res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
-        }
-    })   
-};
+        if (err) throw err;
 
-function questions() {
-    inquirer
-        .prompt({
-            name: "ID",
+        // Creates a table
+        var table = new Table({
+            head: ["Item ID", "Product Name", "Department", "Price", "Stock Quantity"]
+        });
+
+
+        // Displays all items for sale
+        for (var i = 0; i < res.length; i++) {
+            table.push([res[i].item_id, res[i].product_name, res[i].department_name,
+                res[i].price, res[i].stock_quantity
+            ]);
+        }
+        console.log(table.toString());
+
+        //Prompt to buy products
+        inquirer.prompt([{
+            name: "itemId",
             type: "input",
-            message: "Choose the item by typing the ID"
-        })
-        .then (function(answer) {
-            
-        })
+            message: "Please, type the Item ID you would like to purchase?",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }, {
+            name: "quantity",
+            type: "input",
+            message: "How many of this product would you like to buy?",
+            validate: function(value) {
+             if (isNaN(value) === false) {
+                 return true;
+             } else {
+                 return false;
+             }
+            }
+        }]).then(function(answers) {
+            var chosenId = answers.itemId;
+            var chosenQuantity = answers.quantity;
+            purchase(chosenId, chosenQuantity);
+        });
+    });
 };
+///Take order and update DB
+function purchase(ID, quantityNeeded) {
+
+    connection.query("SELECT * FROM products WHERE item_id = " + ID, function(err, res) {
+        if (err) throw err;
+        //
+        if (quantityNeeded <= res[0].stock_quantity) {
+            var totalCost = res[0].price * quantityNeeded;
+
+            console.log("Your total is $" + totalCost + ". Thank you for your purchase!");
+
+            //Update quantity in the DB
+            connection.query("UPDATE products SET stock_quantity = stock_quantity - " + quantityNeeded + " WHERE item_id = " + ID);
+        
+        } else {
+
+            console.log("We don't have enough of that item to fulfill your order.");
+        };
+        //Callback to displayProducts function.
+        displayProducts();
+    })
+}
+
+//Callback to displayProducts function.
+displayProducts();
 
